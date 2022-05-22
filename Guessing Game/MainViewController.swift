49f897efb522ad450648368ea81a0df3a626d44f
ckost9n <ckost9n@gmail.com>
2022-05-22 21:53:58 +0300

    Delete Storyboard

--- Guessing Game/MainViewController.swift
+++ Guessing Game/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Guessing Game
 //
 //  Created by Konstantin on 22.05.2022.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
